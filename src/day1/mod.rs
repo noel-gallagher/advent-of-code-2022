b3f530dbb9168a87b009c095acac66236f33d656
noel-gallagher <70592626+noel-gallagher@users.noreply.github.com>
2022-12-04 14:40:15 +0000

    add missing tests

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -17,22 +17,62 @@ fn sort(highest_calorie: u32, (one, two, three): (u32, u32, u32)) -> (u32, u32, 
     }
 }
 
+fn solve(input: &str) -> (u32, u32, u32) {
+    input.split(SINGLE_ELF_DELIM).fold((0, 0, 0), |acc, elf| {
+        let highest_calorie = elf
+            .split(ITEM_DELIM)
+            .fold(0, |acc, item| acc + item.parse::<u32>().unwrap_or(0));
+        sort(highest_calorie, acc)
+    })
+}
+
+fn part_one(input: &str) -> u32 {
+    let (highest, _, _) = solve(input);
+    highest
+}
+
+fn part_two(input: &str) -> u32 {
+    let (highest, second_highest, third_highest) = solve(input);
+    highest + second_highest + third_highest
+}
+
 pub fn day1() {
     let file_path = env::current_dir().unwrap().join(FILE_PATH);
 
     let file_contents = fs::read_to_string(file_path).unwrap();
 
-    let (highest, second_highest, third_highest) =
-        file_contents
-            .split(SINGLE_ELF_DELIM)
-            .fold((0, 0, 0), |acc, elf| {
-                let highest_calorie = elf
-                    .split(ITEM_DELIM)
-                    .fold(0, |acc, item| acc + item.parse::<u32>().unwrap_or(0));
-                sort(highest_calorie, acc)
-            });
-    let sum = highest + second_highest + third_highest;
+    println!("Day 1 - Part 1: {}", part_one(&file_contents));
+    println!("Day 1 - Part 2: {}", part_two(&file_contents));
+}
 
-    println!("Day 1 - Part 1: {}", highest);
-    println!("Day 1 - Part 2: {}", sum);
+#[cfg(test)]
+mod test {
+    use crate::day1::{part_one, part_two};
+
+    const TEST_INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    #[test]
+    fn example_input_part_1() {
+        let expected_highest_calories = 24000;
+        assert_eq!(part_one(TEST_INPUT), expected_highest_calories)
+    }
+
+    #[test]
+    fn example_input_part_2() {
+        let expected_highest_calories = 45000;
+        assert_eq!(part_two(TEST_INPUT), expected_highest_calories)
+    }
 }
